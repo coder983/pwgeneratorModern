6377104368fb60f155244a575625265646cade60
coder983 <coder983@protonmail.com>
2020-05-07 20:47:39 -0400

    Initial Entry

---
+++ src/main/java/edu/aws/pwgenerator/PwgeneratorApplication.java
@@ -0,0 +1,13 @@
+package edu.aws.pwgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwgeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PwgeneratorApplication.class, args);
+    }
+
+}
