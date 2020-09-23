0570d123b7d08d655e3978f7235225fe8dfbe77d
coder983 <coder983@protonmail.com>
2020-09-23 11:54:17 -0400

    Syncing with new Computoe

--- src/main/java/edu/aws/pwgenerator/PwGeneratorController.java
+++ src/main/java/edu/aws/pwgenerator/PwGeneratorController.java
@@ -8,21 +8,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class PwGeneratorController {
 
-    @Autowired
-    PwGeneratorService service;
-
-    @Autowired
-    NamesRepository namesRepository;
-
-    @Autowired
-    PasswordDataLoader loader;
+    private final PwGeneratorService service;
+    private final NamesRepository namesRepository;
+    private final PasswordDataLoader loader;
 
     StatusManager initializer = new StatusManager();
+
+    public PwGeneratorController(PwGeneratorService service, NamesRepository namesRepository, PasswordDataLoader loader) {
+        this.service = service;
+        this.namesRepository = namesRepository;
+        this.loader = loader;
+    }
 
     @RequestMapping
     public String showMain(){
